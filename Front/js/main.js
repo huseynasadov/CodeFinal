e9ba5c45458d1438be8f460f1a17b4a7ac2cea2e
huseynasadov <Huseynia@code.edu.az>
2019-12-13 21:21:21 +0400

    Asp Core Project created home,shop.blog controllers added

--- Front/js/main.js
+++ Front/js/main.js
@@ -20,7 +20,7 @@
     /*---stickey menu---*/
     $(window).on('scroll',function() {    
            var scroll = $(window).scrollTop();
-           if (scroll < 100) {
+           if (scroll < 170) {
             $(".sticky-header").removeClass("sticky");
            }else{
             $(".sticky-header").addClass("sticky");
